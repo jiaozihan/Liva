cbb76cf348a0d630702eb0877949365cf7fe537d
Yanan Zhang <zyntop2014@gmail.com>
2016-07-25 16:19:56 -0400

    updates README

---
+++ src/utils.ml
@@ -0,0 +1,23 @@
+(* Pretty Printer *)
+open Ast
+open Sast
+open Parser
+
+
+let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+
+let string_of_fdecl fdecl =
+  string_of_typ fdecl.returnType  ^ " " ^
+  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
+  ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "}\n"
+
+let string_of_program (vars, funcs) =
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_fdecl funcs)
+
+
+
+
